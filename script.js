47c72465e941bc85a91574363fec3ad38010076b
Itsbitscoder <154059626+Itsbitscoder@users.noreply.github.com>
2024-01-25 02:06:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ var typed = new Typed(".text", {
     backDelay: 2000,
     loop: true
 });
+
+const bx = document.querySelector('.icon')
+const cros = document.querySelector('.icon i')
+const navbar2 = document.querySelector('.navbar-2')
+
+bx.onclick = function () {
+    navbar2.classList.toggle('open')
+
+    const isopen = navbar2.classList.contains('open')
+
+    cros.classList = isopen
+        ? 'fa-solid fa-xmark bx-menu'
+        : 'fa-solid fa-bars bx-menu'
+}
